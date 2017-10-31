153862f2ee0adb4bb6ba5b0bacb89846c6bd350e
Alena Lipinskaya <ALipinskaya@iba.by>
2017-10-31 12:31:11 +0300

    creating three projects: console, class library and test. Set app config in console project

---
+++ Lab4/Lab4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab4_XmlTechnologies;
+using System.IO;
+using System.Linq;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream[] xmlFiles = Directory.GetFiles(Lab4.Properties.Settings.Default.XmlFolderPath).Select(File.OpenRead).ToArray();
+            
+        }
+    }
+}
---
+++ Lab4_XmlTechnologiesTest/Lab4_XmlTechnologiesTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab4_XmlTechnologies;
+using System.IO;
+using System.Linq;
+
+namespace Lab4_XmlTechnologiesTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GetFiles()
+        {
+           
+        }
+    }
+}
