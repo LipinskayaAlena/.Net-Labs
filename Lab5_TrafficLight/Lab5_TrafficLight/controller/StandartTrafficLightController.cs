0a5ed40de204acf0b19f4e58606dd5d56691f651
Alena Lipinskaya <ALipinskaya@iba.by>
2017-11-18 18:04:46 +0300

    change lights standarttrafficlight

--- Lab5_TrafficLight/Lab5_TrafficLight/controller/StandartTrafficLightController.cs
+++ Lab5_TrafficLight/Lab5_TrafficLight/controller/StandartTrafficLightController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using Lab5_TrafficLight.controller;
 
 namespace Lab5_TrafficLight.controller
@@ -14,14 +15,42 @@ namespace Lab5_TrafficLight.controller
         public const string YELLOW_COLOR = "YELLOW";
 
         public const string GREEN_COLOR = "GREEN";
-        
+
+        private static Timer timer;
+
+        private LinkedList<string> lights = new LinkedList<string>(new string[] { RED_COLOR, YELLOW_COLOR, GREEN_COLOR });
+        LinkedListNode<string> current_light;
+
+        bool isEnabled;
         public void TurnOn(ChangerLightEvent changerLightEvent)
         {
-            changerLightEvent.SimulateEvent("RED");
+            timer = new Timer(3000);
+            isEnabled = true;
 
-            changerLightEvent.SimulateEvent("YELLOW");
+            current_light = lights.First;
+            timer.Elapsed += (sender, e) => SwitchLight(sender, e, changerLightEvent);
+            
+            timer.Start();
+            
+        }
 
-            changerLightEvent.SimulateEvent("GREEN");
+        private void SwitchLight(object source, ElapsedEventArgs e, ChangerLightEvent changerLightEvent )
+        {
+            changerLightEvent.SimulateEvent(current_light.Value);
+            if(isEnabled && current_light.Next != null)
+            {
+                current_light = current_light.Next;
+            } else if(!isEnabled && current_light.Previous != null)
+            {
+                current_light = current_light.Previous;
+            } else
+            {
+                isEnabled = !(isEnabled);
+            }
+                
+            
+            
+                
         }
 
         public new string GetType()
