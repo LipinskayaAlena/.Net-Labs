ead9a78cc9ab9908e72c68277c1b4dc84a58aeaf
Alena Lipinskaya <ALipinskaya@iba.by>
2017-12-01 16:08:29 +0300

    create database and create model from

---
+++ BankAccount/BankAccount/BankAccount.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Account]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Type] NCHAR(20) NOT NULL, 
+    [FistName] NCHAR(20) NOT NULL, 
+    [LastName] NCHAR(20) NOT NULL, 
+    [Date] DATE NOT NULL, 
+    [Account] MONEY NOT NULL, 
+    [BonusPoints] MONEY NOT NULL
+)
